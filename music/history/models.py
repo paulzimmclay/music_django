f28dcdb15e0d9637bc7c336a0c4f2341567e7215
pzc <paul.zimmerman.clayton@gmail.com>
2018-07-20 14:15:31 -0500

    adding models

--- music/history/models.py
+++ music/history/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    artist_name = models.CharField(max_length=100)
+    song_id = models.IntegerField
+
+
+class Song(models.Model):
+    song_name = models.CharField(mox_length=100)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
